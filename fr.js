9c0164b98220ebaec2cc2ae9ca5c44024a05f77d
Gangadhar Havani <gangadhar.havani33@gmail.com>
2022-10-06 13:55:25 +0530

    commit message

---
+++ fr.js
@@ -0,0 +1,7 @@
+var count=0;
+var op=0;
+for(i=0;i<5;i++)
+{
+    op = prompt("Enter Value");
+    document.write(op);
+}
---
+++ while.js
@@ -0,0 +1,12 @@
+
+var count=0;
+//var msg="";
+var op=0;
+while(count<5)
+{
+    op= prompt("Enter Value");
+    //msg=msg+op;
+    count++;
+    document.write(op);
+    
+}
