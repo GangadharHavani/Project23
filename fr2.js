ee09383fdece9dbe9b38cc5418dd78e78b613ff6
Gangadhar Havani <92570878+GangadharHavani@users.noreply.github.com>
2022-10-07 12:04:19 +0530

    Delete fr2.js

--- fr2.js
+++
@@ -1,8 +0,0 @@
-var count=0;
-var val=0, totl=0;
-for(count=0;count<5;count++)
-{
-    val = prompt(" Enter the marks");
-    totl=totl+parseInt(val);
-}
-document.write("The Total marks obtained are: "+totl);
